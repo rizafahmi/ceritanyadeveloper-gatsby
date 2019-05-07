501a673ee4c482ca1feea55acbe38284828dc4a7
Riza Fahmi <rizafahmi@gmail.com>
2019-05-07 10:42:33 +0700

    Setup tailwind and basic layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
         name: `episodes`,
         path: `${__dirname}/src/episodes`
       }
-    }
+    },
+    `gatsby-plugin-postcss`
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
+import './index.css';
+
 const TitleAndDescription = ({ data }) => {
   const { title, description } = data.site.siteMetadata;
 
   return (
-    <div>
+    <div className="">
       <h1>{title}</h1>
       <p>{description}</p>
+      <p>
+        Tersedia di:{' '}
+        <a
+          href="https://itunes.apple.com/us/podcast/ceritanya-developer-podcast/id1364448110?mt=2&uo=4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          iTunes
+        </a>
+        ,{' '}
+        <a
+          href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8zMDM0OGQ0L3BvZGNhc3QvcnNz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Google Podcast
+        </a>
+        ,{' '}
+        <a
+          href="https://open.spotify.com/show/6grT1c7jDkhK4skm1YIsTs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Spotify
+        </a>
+        ,{' '}
+        <a
+          href="https://anchor.fm/ceritanya-developer"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Anchor.fm
+        </a>
+        ,{' '}
+        <a href="http://pca.st/CwU8" target="_blank" rel="noopener noreferrer">
+          Pocket Cast
+        </a>
+        , dan berbagai <em>platform</em> lainnya.
+      </p>
     </div>
   );
 };
@@ -32,34 +73,34 @@ const Header = () => {
 
 const Layout = () => {
   return (
-    <div className="hack">
-      <div className="container">
-        <div className="grid">
-          <main className="cell">
+    <div className="container font-mono mx-auto leading-loose text-grey-darkest">
+      <div className="header flex flex-col">
+        <div className="logo my-2 flex flex-row">
+          <div className="image mx-4">
             <img
               src={'images/logo.png'}
               alt="Logo Ceritanya Developer"
               style={{ width: 256 }}
             />
-            <nav class="nav">
-              <ul className="nav-list" style={{ listStyleType: 'none' }}>
-                <li className="menuitem">
-                  <a href="/about">Tentang</a>
-                </li>
-                <li className="menuitem">
-                  <a href="/newsletter">Newsletter</a>
-                </li>
-                <li className="menuitem">
-                  <a href="/donate">Donasi</a>
-                </li>
-                <li className="menuitem">
-                  <a href="mailto:rizafahmi@gmail.com">Kontak</a>
-                </li>
-              </ul>
-            </nav>
-            <Header />
-          </main>
+          </div>
+          <Header />
         </div>
+        <nav class="nav">
+          <ul className="nav-list list-reset flex justify-around my-2">
+            <li className="menuitem">
+              <a href="/about">Tentang</a>
+            </li>
+            <li className="menuitem font-mono">
+              <a href="/newsletter">Newsletter</a>
+            </li>
+            <li className="menuitem">
+              <a href="/donate">Donasi</a>
+            </li>
+            <li className="menuitem">
+              <a href="mailto:rizafahmi@gmail.com">Kontak</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
