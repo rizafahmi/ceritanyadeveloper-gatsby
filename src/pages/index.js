f8a701f67fcc2e85d1c9fb51fa3fdcb5559cad0e
Riza Fahmi <rizafahmi@gmail.com>
2019-08-08 07:49:57 +0700

    Add episode Ego [1][4]

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import './index.css';
 const Layout = ({ data }) => {
   const { nodes } = data.allMarkdownRemark;
   return (
-    <div className="container font-mono mx-auto leading-loose text-black">
+    <div className="max-w-lg container font-mono mx-auto leading-loose text-black">
       <Seo
         data={{
           frontmatter: { title: 'Ceritanya Developer Podcast' },
